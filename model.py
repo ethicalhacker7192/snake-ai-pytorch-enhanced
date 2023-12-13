dcad7f7f0673c193a61ca20c34ca192e07648c98
nope! no name for you to hear! none at all! <107788660+ethicalhacker7192@users.noreply.github.com>
2023-12-13 09:23:04 -0700

    Update model.py

--- model.py
+++ model.py
@@ -8,13 +8,13 @@ class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size) # Here we added another hidden layer, often more layers means better outputs, tread with care, too many layers can cause overfitting.
         self.linear3 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear1(x)) # ReLU activations introduce non-linearity in the model, we do this to both to introduce randomness.
         x = F.relu(self.linear2(x))
-        x = self.linear3(x)
+        x = self.linear3(x) # No ReLU here because we want this one to decide off of the past layers.
         return x
 
     def save(self, file_name='model.pth'):
@@ -59,10 +59,12 @@ class QTrainer:
                 Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
 
             target[idx][torch.argmax(action[idx]).item()] = Q_new
-    
-        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
-        # pred.clone()
-        # preds[argmax(action)] = Q_new
+
+        if not done:
+            Q_new = r + y * max(next_predicted Q value) # -> only do this if not done
+            pred.clone()
+            preds[argmax(action)] = Q_new
+        
         self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()

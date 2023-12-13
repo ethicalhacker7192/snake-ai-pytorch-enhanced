75911778acbe96610ae5ece5c6519d5083cd39c0
nope! no name for you to hear! none at all! <107788660+ethicalhacker7192@users.noreply.github.com>
2023-12-13 09:33:05 -0700

    Update model.py

--- model.py
+++ model.py
@@ -61,7 +61,7 @@ class QTrainer:
             target[idx][torch.argmax(action[idx]).item()] = Q_new
 
         if not done:
-            Q_new = r + y * max(next_predicted Q value) # -> only do this if not done
+            Q_new = r + y * max(next_predicted, Q_new) # -> only do this if not done
             pred.clone()
             preds[argmax(action)] = Q_new
         
